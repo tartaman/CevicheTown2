a75bf08cd0cb84b0c6725c3de292f55655e0c96a
PlayerAdjustments <ferzg2004@gmail.com>
2024-05-26 16:39:48 -0600

    Json Saving

--- CevicheTown/Assets/JsonTest/GameManager.cs
+++ CevicheTown/Assets/JsonTest/GameManager.cs
@@ -1,16 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debug.LogWarning("Saving grid into json");
-            this.GetComponent<SaveData>().SaveIntoJson();
-        }
-    }
+
 }
